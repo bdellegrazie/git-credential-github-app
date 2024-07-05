98ae0573f92770f339e0449dcebe415a936cead7
Brett Delle Grazie <brett.dellegrazie@gmail.com>
2024-07-05 23:11:39 +0200

    feat: support slug as well as organisation

--- git-credential-github-app.go
+++ git-credential-github-app.go
@@ -19,6 +19,7 @@ type githubApp struct {
 	privateKey     *rsa.PrivateKey
 	installationId uint64
 	organisation   string
+	slug           string
 }
 
 func PrivateKeyFromPath(keyFile string) (*rsa.PrivateKey, error) {
@@ -70,6 +71,30 @@ func GithubAppOrgGetInstallation(app *githubApp, token string, client *http.Clie
 	return dat.Id, nil
 }
 
+type GithubAppSlugGetInstallationResponseBody struct {
+	Id uint64 `json:"id"`
+}
+
+func GithubAppSlugGetInstallation(app *githubApp, token string, client *http.Client) (uint64, error) {
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/repos/%s/installation", app.url, app.slug), nil)
+	if err != nil {
+		return 0, err
+	}
+	GithubAppCommonHeaders(req, token)
+	resp, err := client.Do(req)
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+
+	var dat GithubAppSlugGetInstallationResponseBody
+	decoder := json.NewDecoder(resp.Body)
+	if err = decoder.Decode(&dat); err != nil {
+		return 0, err
+	}
+	return dat.Id, nil
+}
+
 type GithubAppGetInstallationAccessTokenResponseBody struct {
 	Token     string `json:"token"`
 	ExpiresAt string `json:"expires_at"` // "expires_at": "2016-07-11T22:14:10Z"
@@ -109,7 +134,7 @@ func GithubAppGetInstallationAccessToken(app *githubApp, token string, client *h
 func PrintUsage() {
 	fmt.Fprintln(os.Stderr, "Usage:")
 	fmt.Fprintln(os.Stderr, os.Args[0], "-h|--help")
-	fmt.Fprintln(os.Stderr, os.Args[0], "<-username USERNAME> <-clientId ID> <-privateKey PATH_TO_PRIVATE_KEY> <-installationId INSTALLATION_ID | -organisation ORGANISATION> [-url GITHUB_API_URL] <get|store|erase>", os.Args[0])
+	fmt.Fprintln(os.Stderr, os.Args[0], "<-username USERNAME> <-clientId ID> <-privateKey PATH_TO_PRIVATE_KEY> <-installationId INSTALLATION_ID | -organisation ORGANISATION | -slug OWNER/REPO> [-url GITHUB_API_URL] <get|store|erase>", os.Args[0])
 	flag.PrintDefaults()
 }
 
@@ -118,7 +143,8 @@ func main() {
 	flag.StringVar(&app.clientId, "clientId", "", "GitHub App ClientId (preferred) or AppId, mandatory")
 	privateKeyFile := flag.String("privateKey", "", "GitHub App Private Key File, mandatory")
 	flag.Uint64Var(&app.installationId, "installationId", 0, "GitHub App Installation Id, recommended")
-	flag.StringVar(&app.organisation, "organisation", "", "GitHub App Organisation, required if InstallationId not supplied")
+	flag.StringVar(&app.organisation, "organisation", "", "GitHub App Organisation, required if InstallationId not supplied and installation is an org")
+	flag.StringVar(&app.slug, "slug", "", "GitHub App Owner/Repo slug, required if InstallationId not supplied and installation is a specific repo")
 	flag.StringVar(&app.url, "url", "https://api.github.com", "GitHub API Base URL")
 	usernamePtr := flag.String("username", "", "GitHub Username, mandatory, recommend GitHub App Name")
 
@@ -138,11 +164,13 @@ func main() {
 	}
 
 	if app.installationId == 0 {
-		// Get InstallationId from the organisation
-		if len(app.organisation) == 0 {
-			panic("When InstallationId is not supplied, Organisation is required")
-		} else {
+		// Get InstallationId
+		if len(app.organisation) > 0 {
 			app.organisation = strings.ToLower(app.organisation)
+		} else if len(app.slug) > 0 {
+			app.slug = strings.ToLower(app.slug)
+		} else {
+			panic("When InstallationId is not supplied, Organisation or Slug is required")
 		}
 	}
 
@@ -175,10 +203,20 @@ func main() {
 	}
 
 	if app.installationId == 0 {
-		app.installationId, err = GithubAppOrgGetInstallation(&app, token, http.DefaultClient)
-		if err != nil {
-			fmt.Println("quit=1")
-			panic("Could not get installation ID from Organisation")
+		if len(app.organisation) > 0 {
+			app.installationId, err = GithubAppOrgGetInstallation(&app, token, http.DefaultClient)
+			if err != nil {
+				fmt.Println("quit=1")
+				panic("Could not get installation ID from Organisation")
+			}
+		} else if len(app.slug) > 0 {
+			app.installationId, err = GithubAppSlugGetInstallation(&app, token, http.DefaultClient)
+			if err != nil {
+				fmt.Println("quit=1")
+				panic("Could not get installation ID from Slug")
+			}
+		} else {
+			panic("Neither Organisation nor Slug available")
 		}
 	}
 
